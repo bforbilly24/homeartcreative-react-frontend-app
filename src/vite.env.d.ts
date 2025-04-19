b3365d197d092f00eac45afe5d5be3e2d1c89f66
Halim <halimputra0701@gmail.com>
2025-04-19 14:50:32 +0700

    refactor(files): update as need

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly MODE: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
 import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  assetsInclude: ['**/*.glb', '**/*.png'], 
-  server: {
-    fs: {
-      allow: ['.'],
+
+      // fix loading all icon chunks in dev mode
+      // https://github.com/tabler/tabler-icons/issues/1233
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
 })
